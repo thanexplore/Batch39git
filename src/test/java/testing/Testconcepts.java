1f1e4dfbcc14fafc1930801906a98fd24853f97e
than <thanexplore@gmail.com>
2025-03-30 19:47:04 +0530

    sample maven project

---
+++ src/test/java/testing/Testconcepts.java
@@ -0,0 +1,11 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class Testconcepts {
+
+    @Test
+    public void test2(){
+        System.out.println("I am learning programming");
+    }
+}
