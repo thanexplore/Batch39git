2fc96b79ee77a80fb473354b7080b5e7b64e9e78
Nancy <nancymathew0795@gmail.com>
2025-04-06 09:08:45 -0400

    Number of Occurrences of Specified Character & Student grade

---
+++ src/test/java/nancytesting/GradeCalculator.java
@@ -0,0 +1,36 @@
+package nancytesting;
+
+public class GradeCalculator {
+
+    public static void main(String[] args) {
+        int marks[] = {92,91,98};
+        int attendance=88;
+        String name="John";
+        float total=0, avg;
+
+        for(int i=0; i<3; i++) {
+            total = total + marks[i];
+        }
+        avg = (total/300)*100;
+        System.out.println("Student Name:" + name);
+        System.out.println("Student Mark:" + total);
+        System.out.println("student Attendance:" + attendance);
+        System.out.print("Student Grade: ");
+        if(avg>=90 && attendance>=85)
+        {
+            System.out.print("A");
+        }
+        else if(avg>=80 && avg<=89 && attendance>=75)
+        {
+            System.out.print("B");
+        }
+        else if(avg>=70 && avg<=79 && attendance>=65)
+        {
+            System.out.print("C");
+        }
+        else
+        {
+            System.out.print("D");
+        }
+    }
+}
